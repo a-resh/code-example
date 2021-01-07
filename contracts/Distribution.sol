332a8259a1313e7d6e6ecf3f606ec1a1b7028d6a
Stanislav Obrazcoff <obrazcoff@gmail.com>
2021-01-07 14:24:43 +0100

    Add token and distibution

---
+++ contracts/Distribution.sol
@@ -0,0 +1,73 @@
+pragma solidity 0.5.16;
+
+contract TotemDistribution {
+
+    TotemToken totemToken;
+    address public CommunityDevelopment;
+    address public StakingRewards;
+    address public LiquidityPool;
+    address public PublicSale;
+    address public Advisors;
+    address public SeedInvestment;
+    address public PrivateSale;
+    address public TeamAllocation;
+
+    address payable owner;
+
+    constructor() { owner = msg.sender; }
+
+        modifier onlyOwner {
+        require(
+            msg.sender == owner,
+            "Only owner can call this function."
+        );
+        _;
+    }
+
+    function setTokenAddress(address _address) _onlyOwner {
+        totemToken = TotemToken(_address);
+    }
+
+    function setAddressCommunityDevelopment(address _address) public _onlyOwner {
+        CommunityDevelopment = _address;
+    }
+
+    function setAddressLiquidityPool(address _address) public _onlyOwner {
+        LiquidityPool = _address;
+    }
+
+    function setAddressPublicSale(address _address) public _onlyOwner {
+        PublicSale = _address;
+    }
+
+    function setAddressAdvisors(address _address) public _onlyOwner {
+        Advisors = _address;
+    }
+
+    function setAddressSeedInvestment(address _address) public _onlyOwner {
+        SeedInvestment = _address;
+    }
+
+    function setAddressPrivateSale(address _address) public _onlyOwner {
+        PrivateSale = _address;
+    }
+
+    function setAddressTeamAllocation(address _address) public _onlyOwner (bool success){
+        TeamAllocation = _address;
+    }
+
+    function distibuteTokens () public _onlyOwner {
+      totemToken.transfer(CommunityDevelopment, totemToken.CommunityDevelopment);
+      totemToken.transfer(LiquidityPool, totemToken.LiquidityPool);
+      totemToken.transfer(PublicSale, totemToken.PublicSale);
+      totemToken.transfer(Advisors, totemToken.Advisors);
+      totemToken.transfer(SeedInvestment, totemToken.SeedInvestment);
+      totemToken.transfer(PrivateSale, totemToken.PrivateSale);
+      totemToken.transfer(TeamAllocation, totemToken.TeamAllocation);
+      return true;
+    }
+
+    function getVal() constant public {
+        return totemToken.val();
+    }
+}
--- contracts/TotemToken.sol
+++ contracts/TotemToken.sol
@@ -10,6 +10,36 @@ contract TotemToken {
     string  public standard = "Totem Token v1.0";
     uint256 public totalSupply;
 
+    uint256 public CommunityDevelopment;
+    uint256 public StakingRewards;
+    uint256 public LiquidityPool;
+    uint256 public PublicSale;
+    uint256 public Advisors;
+    uint256 public SeedInvestment;
+    uint256 public PrivateSale;
+    uint256 public TeamAllocation;
+
+    uint8 COMMUNITY_DEVELOPMENT = 100; // 10% for Community development
+    uint8 STAKING_REWARDS = 165; // 16,5% for Staking Revawards
+    uint8 LIQUIDITY_POOL = 25; // 2,5% for Liquidity pool​
+    uint8 PUBLIC_SALE = 250; // 25% for Public Sale
+    uint8 ADVISORS = 50; // 5% for Advisors
+    uint8 SEED_INVESTMENT = 110; // 11% for Seed investment
+    uint8 PRIVATE_SALE = 150; // 15% for Private Sale
+    uint8 TEAM_ALLOCATION = 150; // 15% for Team allocation
+
+    address payable owner;
+
+    constructor() { owner = msg.sender; }
+
+        modifier onlyOwner {
+        require(
+            msg.sender == owner,
+            "Only owner can call this function."
+        );
+        _;
+    }
+    
     event Transfer(
         address indexed _from,
         address indexed _to,
@@ -28,6 +58,16 @@ contract TotemToken {
     function TotemToken (uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
+
+        //Token allocation
+        CommunityDevelopment = ( _initialSupply / 1000 ) * COMMUNITY_DEVELOPMENT;
+        StakingRewards = ( _initialSupply / 1000 ) * STAKING_REWARDS;
+        LiquidityPool = ( _initialSupply / 1000 ) * LIQUIDITY_POOL;
+        PublicSale = ( _initialSupply / 1000 ) * PUBLIC_SALE;
+        Advisors = ( _initialSupply / 1000 ) * ADVISORS;
+        SeedInvestment = ( _initialSupply / 1000 ) * SEED_INVESTMENT;
+        PrivateSale = ( _initialSupply / 1000 ) * PRIVATE_SALE;
+        TeamAllocation = ( _initialSupply / 1000 ) * TEAM_ALLOCATION;
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
@@ -49,7 +89,7 @@ contract TotemToken {
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) public _onlyOwner returns (bool success) {
         require(_value <= balanceOf[_from]);
         require(_value <= allowance[_from][msg.sender]);
 
