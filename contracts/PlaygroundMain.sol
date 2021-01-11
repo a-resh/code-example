6d79a4799031aacc13cbdcd14ebceefdc64a20ce
Stanislav Obrazcoff <obrazcoff@gmail.com>
2021-01-11 12:49:53 +0100

    Add constants

--- contracts/PlaygroundMain.sol
+++ contracts/PlaygroundMain.sol
@@ -10,6 +10,18 @@ contract FactoryDraw {
     uint256 currentDraw_30;
     uint256 currentDraw_60;
 
+    uint256 constant maxPoolSize_15 = 125000;
+    uint256 constant maxPoolSize_30 = 110000;
+    uint256 constant maxPoolSize_60 = 90000;
+
+    uint256 constant totemPrize_15 = 4500;
+    uint256 constant totemPrize_30 = 4000;
+    uint256 constant totemPrize_60 = 5000;
+
+    uint256 constant btcPrize_15 = 100; //0.100 btc 
+    uint256 constant btcPrize_30 = 163; //0.163 btc 
+    uint256 constant btcPrize_60 = 182; //0.182 btc 
+
     constructor() {
       // new draw index will be 0
       currentDraw_15 = -1;
@@ -24,7 +36,7 @@ contract FactoryDraw {
         }
         uint _launchTimeInSec = now + _launchTime * 60 * 60 * 24;
         uint _durationInSec = _launchTimeInSec + _duration * 60 * 60 * 24;
-        Draw newDraw_15 = new Draw(drawId, _launchTimeInSec, _durationInSec);
+        Draw newDraw_15 = new Draw(drawId, _launchTimeInSec, _durationInSec, maxPoolSize_15, totemPrize_15, btcPrize_15);
         Draws_15.push(newDraw_15);
         currentDraw_15 = currentDraw_15 + 1;
       }
@@ -35,7 +47,7 @@ contract FactoryDraw {
         }
         uint _launchTimeInSec = now + _launchTime * 60 * 60 * 24;
         uint _durationInSec = _launchTimeInSec + _duration * 60 * 60 * 24;
-        Draw newDraw_30 = new Draw(drawId, _launchTimeInSec, _durationInSec);
+        Draw newDraw_30 = new Draw(drawId, _launchTimeInSec, _durationInSec, maxPoolSize_30, totemPrize_30, btcPrize_30);
         Draws_30.push(newDraw_30);
         currentDraw_30 = currentDraw_30 + 1;
       }
@@ -46,7 +58,7 @@ contract FactoryDraw {
         }
         uint _launchTimeInSec = now + _launchTime * 60 * 60 * 24;
         uint _durationInSec = _launchTimeInSec + _duration * 60 * 60 * 24;
-        Draw newDraw_60 = new Draw(drawId, _launchTimeInSec, _durationInSec);
+        Draw newDraw_60 = new Draw(drawId, _launchTimeInSec, _durationInSec, maxPoolSize_60, totemPrize_60, btcPrize_60);
         Draws_60.push(newDraw_60);
         currentDraw_60 = currentDraw_60 + 1;
       }
