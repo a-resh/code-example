4148a1b3ac0562948ba78c149e4e87efb09c55f7
Stanislav Obrazcoff <obrazcoff@gmail.com>
2021-02-09 05:06:58 +0100

    Refactor backend config

--- backend/lib/config.js
+++ backend/lib/config.js
@@ -8,20 +8,26 @@ var environments = {};
 
 // Staging (default) environment
 environments.staging = {
-  'httpPort' : 3000,
-  'httpsPort' : 3001,
-  'envName' : 'staging',
-  'hashingSecret' : 'thisIsASecret',
-  'maxChecks' : 5
+  httpPort: 3000,
+  httpsPort: 3001,
+  envName: 'staging',
+  hashingSecret: 'thisIsASecret',
+  maxChecks: 5,
+  btcPriceCheck_1: 'https://api.coindesk.com/v1/bpi/currentprice.json',
+  btcPriceCheck_2: 'https://blockchain.info/ticker',
+  btcPriceCheck_3: 'https://api.coinbase.com/v2/prices/spot?currency=USD',
 };
 
 // Production environment
 environments.production = {
-  'httpPort' : 5000,
-  'httpsPort' : 5001,
-  'envName' : 'production',
-  'hashingSecret' : 'thisIsAlsoASecret',
-  'maxChecks' : 10
+  httpPort: 5000,
+  httpsPort: 5001,
+  envName: 'production',
+  hashingSecret: 'thisIsAlsoASecret',
+  maxChecks: 10,
+  btcPriceCheck_1: 'https://api.coindesk.com/v1/bpi/currentprice.json',
+  btcPriceCheck_2: 'https://blockchain.info/ticker',
+  btcPriceCheck_3: 'https://api.coinbase.com/v2/prices/spot?currency=USD',
 };
 
 // Determine which environment was passed as a command-line argument
