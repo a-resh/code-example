448a2011f876a7fb25d1aace65e91b2617ad0c4b
Stanislav Obrazcoff <obrazcoff@gmail.com>
2021-03-02 13:23:43 +0100

    Update users route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 const USERS = require('../data/users');
 
-router.get('/', (req, res, next) => {
-  res.json(USERS);
+// db
+const User = require('../models/user');
+
+router.get('/', async (req, res, next) => {
+  try {
+    let users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.log('Get users error', error);
+  }
 });
 
 module.exports = router;
